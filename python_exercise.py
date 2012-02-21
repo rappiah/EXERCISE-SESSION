c0d13e2e6c5e0eb1d39f50598f138275f58fc885
Rita <ritaappiah003@gmail.com>
2012-02-21 21:51:37 +0100

    first commit

---
+++ python_exercise.py
@@ -0,0 +1,22 @@
+import os
+import glob
+def files(path):
+   listing = os.listdir(path)
+   return listing
+def filestring(line)
+    search='N'
+    s=line.find(search)
+    if s>=0
+       k=True
+   else:
+       k=False
+    return b;
+def replace(line)
+    nline = line.replace("N','M')
+return nline;
+path = 'cleandata'
+filelist=files(path)
+for infile in filelist:
+	print("current file is: " + infile)
+
+
